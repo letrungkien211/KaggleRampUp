0d3202d00d9f3f779b7487fc01215a0743c2dcc7
letrungkien211 <letrungkien211@gmail.com>
2018-11-09 21:06:37 +0900

    fix tests cases

--- Transformer/transformer.tests.py
+++ Transformer/transformer.tests.py
@@ -9,8 +9,6 @@ import tensorflow as tf
 import numpy as np
 import inspect
 
-sess = tf.InteractiveSession()
-
 d_model = 256
 dk = 5
 dv = 3
@@ -24,44 +22,51 @@ Nx = 1
 
 class TestTransformer(unittest.TestCase):
     def test_scale_product(self):
-        Q = tf.placeholder(tf.float32, shape=(None, Ty, dk))
-        K = tf.placeholder(tf.float32, shape=(None, Tx, dk))
-        V = tf.placeholder(tf.float32, shape=(None, Tx, dv))
-       
-        output = scale_dot_product(Q, K, V, Tx, dk)
+        tf.reset_default_graph();
+        with tf.Session() as sess:
+            Q = tf.placeholder(tf.float32, shape=(None, Ty, dk))
+            K = tf.placeholder(tf.float32, shape=(None, Tx, dk))
+            V = tf.placeholder(tf.float32, shape=(None, Tx, dv))
         
-        q_val = np.random.rand(B, Ty, dk)
-        k_val = np.random.rand(B, Tx, dk)
-        v_val = np.random.rand(B, Tx, dv)
+            output = scale_dot_product(Q, K, V, Tx, dk)
+            
+            q_val = np.random.rand(B, Ty, dk)
+            k_val = np.random.rand(B, Tx, dk)
+            v_val = np.random.rand(B, Tx, dv)
 
-        out_val = output.eval(feed_dict={Q: q_val, K:k_val, V: v_val})
-        print(inspect.stack()[0].function, '---')
-        print(out_val.shape)
-        print(out_val)
-        assert all([x == out_val.shape[i] for i, x in enumerate([B, Ty, dv])])
+            out_val = output.eval(feed_dict={Q: q_val, K:k_val, V: v_val})
+            print(inspect.stack()[0].function, '---')
+            print(out_val.shape)
+            print(out_val)
+            assert all([x == out_val.shape[i] for i, x in enumerate([B, Ty, dv])])
 
     def test_mask(self):
-        x = tf.placeholder(tf.float32, shape=(None, Ty, Ty))
-        y = mask(x, Ty, Ty-2)
-        out_val = y.eval(feed_dict={x: np.random.rand(B, Ty, Ty)})
-        print(inspect.stack()[0].function, '---')
-        print(out_val.shape)
-        print(out_val)       
+
+        tf.reset_default_graph();
+        with tf.Session() as sess:
+            x = tf.placeholder(tf.float32, shape=(None, Ty, Ty))
+            y = mask(x, Ty, Ty-2)
+            out_val = y.eval(feed_dict={x: np.random.rand(B, Ty, Ty)})
+            print(inspect.stack()[0].function, '---')
+            print(out_val.shape)
+            print(out_val)       
 
     def test_multi_head(self):
-        multiHead = MultiHeadAttention(d_model, dk, dv, h)
-        input = tf.placeholder(tf.float32, shape=(None, Tx, d_model))
+        tf.reset_default_graph();
+        with tf.Session() as sess:
+            multiHead = MultiHeadAttention(d_model, dk, dv, h)
+            input = tf.placeholder(tf.float32, shape=(None, Tx, d_model))
 
-        output = multiHead.forward(input,input,input, Tx, maskStep)
+            output = multiHead.forward(input,input,input, Tx, maskStep)
 
-        sess.run(tf.global_variables_initializer())
-        input_val = np.random.rand(B, Tx, d_model)
-        out_val = output.eval(feed_dict={input: input_val})
+            sess.run(tf.global_variables_initializer())
+            input_val = np.random.rand(B, Tx, d_model)
+            out_val = output.eval(feed_dict={input: input_val})
 
-        print(inspect.stack()[0].function, '---')
-        print(out_val.shape)
-        print(out_val)  
-        assert all([x == out_val.shape[i] for i, x in enumerate([B, Tx, d_model])])      
+            print(inspect.stack()[0].function, '---')
+            print(out_val.shape)
+            print(out_val)  
+            assert all([x == out_val.shape[i] for i, x in enumerate([B, Tx, d_model])])      
 
     def test_encoder_model(self):
         tf.reset_default_graph()
